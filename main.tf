5cb2eab93013904a7e106d898222b0cef35d8b6a
Victor Daniel Muñoz Muñoz <78627393+vmunozm@users.noreply.github.com>
2024-09-27 11:28:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -552,3 +552,18 @@ module "sqs" {
     Environment = "dev"
   }
 }
+
+module "s3_bucket_for_logs" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket-for-logs"
+  acl    = "log-delivery-write"
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  attach_elb_log_delivery_policy = true
+}
