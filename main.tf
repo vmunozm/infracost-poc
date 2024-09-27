6563d3d7bd5bd6ac3012ee3f79ee94b90d90dfcd
Victor Daniel Muñoz Muñoz <78627393+vmunozm@users.noreply.github.com>
2024-09-27 10:59:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "aws_ecs_service" "mongo" {
   name            = "mongodb"
   cluster         = aws_ecs_cluster.foo.id
   task_definition = aws_ecs_task_definition.mongo.arn
-  desired_count   = 3
+  desired_count   = 400
   iam_role        = aws_iam_role.foo.arn
   depends_on      = [aws_iam_role_policy.foo]
 
